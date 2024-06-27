6b26c1792ebdb964d936e1781f09e7eba18414e8
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-06-27 16:34:08 +0900

    Title: n개의 점 중 m개 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240627/n개의 점 중 m개 고르기/choose-m-out-of-n-points.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Point{
+    int x;
+    int y;
+};
+
+int n, m, ans = 21e8;
+Point arr[21];
+int c[21];
+vector<Point> vec;
+
+void check()
+{
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if (i == j) continue;
+
+            int xdif = arr[c[i]].x - arr[c[j]].x;
+            int ydif = arr[c[i]].y - arr[c[j]].y;
+            int dist = (xdif*xdif) + (ydif*ydif);
+
+            ans = min(ans, dist);
+        }
+    }
+}
+
+void dfs(int depth, int idx)
+{
+    if (depth == m)
+    {
+        check();
+        return;
+    }
+
+    for(int i=idx;i<n;i++)
+    {
+        c[depth] = i;
+        dfs(depth + 1, i + 1);
+    }
+}
+
+int main() {
+    
+    cin >> n >> m;
+
+    for(int i=0;i<n;i++) cin >> arr[i].x >> arr[i].y;
+
+    dfs(0, 0);
+
+    cout << ans;
+
+    return 0;
+}

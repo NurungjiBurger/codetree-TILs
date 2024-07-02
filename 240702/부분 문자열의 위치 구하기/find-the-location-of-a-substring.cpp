4e5c9b4c0fb9e74426176a4834ebd6acabca0620
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-07-02 14:40:21 +0900

    Title: 부분 문자열의 위치 구하기, Time: 233ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240702/부분 문자열의 위치 구하기/find-the-location-of-a-substring.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+string input, goal;
+unordered_map<string, int> um;
+
+int main() {
+
+    cin >> input;
+    cin >> goal;
+
+    for(int i=0;i<input.length();i++)
+    {
+        string str = "";
+        for(int j=i;j<input.length();j++)
+        {
+            str += input[j];
+            if (um.count(str) == 0) um[str] = i;
+        }
+    }
+
+    if (um.count(goal) == 0) cout << -1;
+    else cout << um[goal];
+
+    return 0;
+}

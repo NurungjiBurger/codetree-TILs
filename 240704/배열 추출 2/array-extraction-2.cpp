7307a7941a0fe50cdeadd8a2cb4b613522345992
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-07-04 22:55:57 +0900

    Title: 배열 추출 2, Time: 105ms, Memory: 0MB, Status: Passed - CodeTree

--- 240704/배열 추출 2/array-extraction-2.cpp
+++ 240704/배열 추출 2/array-extraction-2.cpp
@@ -6,6 +6,7 @@ using namespace std;
 struct Num{
     int num;
     bool operator<(Num right) const{
+        if (abs(num) == abs(right.num)) return num > right.num;
         return abs(num) > abs(right.num);
     }
 };

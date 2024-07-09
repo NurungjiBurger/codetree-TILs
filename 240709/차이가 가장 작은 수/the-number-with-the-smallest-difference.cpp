9a434ba79ccf944a36b1f9fc4631b65a1950dda4
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-07-09 16:35:26 +0900

    Title: 차이가 가장 작은 수, Time: 89ms, Memory: 4MB, Status: Passed - CodeTree

--- 240709/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
+++ 240709/차이가 가장 작은 수/the-number-with-the-smallest-difference.cpp
@@ -1,43 +1,36 @@
 #include <iostream>
+#include <algorithm>
 #include <set>
 
 using namespace std;
 
 int n, m, ans = 21e8;
+int arr[100001];
 set<int> s;
 
 int main() {
 
     cin >> n >> m;
 
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) 
     {
         int num;
         cin >> num;
 
         s.insert(num);
+    }
 
-        if (s.size() > 1)
-        {
-            auto iter = s.find(num);
+    for(auto item : s)
+    {
+        // 차이가 m 이상
+        // lowerbound(arr[i] + m)
+        auto iter = s.lower_bound(item - m);
+        
+        if (iter != s.end() && item - *iter >= m) ans = min(ans, abs(*iter - item));
 
-            if (iter != s.begin())
-            {
-                iter--;
-                //cout << "left : " << *iter << " " << num << endl;
-                if (abs(num - *iter) >= m) ans = min(ans, abs(num - *iter));
-            }
+        iter = s.lower_bound(item + m);
 
-            iter = s.find(num);
-
-            iter++;
-
-            if (iter != s.end())
-            {
-                //cout << "right : " << *iter << " " << num << endl;
-                if (abs(num - *iter) >= m) ans = min(ans, abs(num - *iter));
-            }
-        }
+        if (iter != s.end() && *iter - item >= m) ans = min(ans, abs(*iter - item));
     }
 
     if (ans == 21e8) ans = -1;

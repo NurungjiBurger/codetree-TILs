953d12643c4f6bc0fe5558eba971cc6add5330e3
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-07-20 12:40:49 +0900

    Title: 최소 통과 시간, Time: 31ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/최소 통과 시간/minimum-transit-time.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+long long arr[100001], ans;
+
+void ps(long long start, long long end)
+{
+    long long mid;
+
+    while(start <= end)
+    {
+        // 예상되는 시간
+        mid = (start + end) / 2;
+
+        int cnt = 0;
+        for(int i=0;i<m;i++) cnt += (int)(mid / arr[i]);
+
+        if (cnt >= n)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else start = mid + 1;
+    }
+}
+
+
+int main() {
+
+    cin >> n >> m;
+
+    long long mini = 21e13, maxi = -21e13;
+
+    for(int i=0;i<m;i++)
+    {
+        cin >> arr[i];
+
+        mini = min(mini, arr[i]);
+        maxi = max(maxi, arr[i]);
+    }
+
+    ps(mini, maxi * n);
+
+    cout << ans;
+
+
+    return 0;
+}

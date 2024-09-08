e2d96ec3519b088538adbbd533c8f9aaf8a266c7
sangbin_park <44452761+NurungjiBurger@users.noreply.github.com>
2024-09-08 12:23:10 +0900

    Title: 최소 차 분할, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/최소 차 분할/minimum-diff-partition.cpp
+++ 240908/최소 차 분할/minimum-diff-partition.cpp
@@ -1,90 +1,49 @@
 #include <iostream>
+
 using namespace std;
 
-int n;
-int arr[1001];
-int dp[4][1001];
+int n, sum = 0, ans = 21e8;
+int arr[101];
+int dp[100001];
 
 int main() {
     
     cin >> n;
 
-    for(int i=0;i<n;i++) cin >> arr[i];
-
-    // 정확히 두 그룹으로 나뉜다
-    // A or B
-
-    // A에 속하는 경우 B에 속하는 경우
-    // A B A B
-    // 0 X X 0
-
-    dp[0][0] = arr[0];
-    dp[3][0] = arr[0];
-
-    for(int i=i;i<n;i++)
+    for(int i=0;i<n;i++) 
     {
-
-        int diff = 21e8;
-
-        // A에 속하는 경우
-        if (abs(dp[2][i - 1] + arr[i] - dp[3][i - 1]) < abs(dp[0][i - 1] + arr[i] - dp[1][i - 1]))
-        {
-            // 이전에는 B가 가져감
-            dp[0][i] = dp[2][i - 1] + arr[i];
-            dp[1][i] = dp[3][i - 1];
-
-            diff = abs(dp[2][i - 1] + arr[i] - dp[3][i - 1]);
-        }
-        else
-        {
-            // 이전에도 A가 가져감
-            dp[0][i] = dp[0][i - 1] + arr[i];
-            dp[1][i] = dp[1][i - 1];
-
-            diff = abs(dp[0][i - 1] + arr[i] - dp[1][i - 1]);
-        }
-
-        // 이전 기록을 다 합치고 새로 만들어주는게 차이가 적은 경우
-        if (abs(dp[0][i - 1] + dp[1][i - 1] - arr[i]) < diff)
-        {
-            dp[0][i] = dp[0][i - 1] + dp[1][i - 1];
-            dp[1][i] = arr[i];
-        }
-
-
-        // B에 속하는 경우
-        if (abs(dp[1][i - 1] + arr[i] - dp[0][i - 1]) < abs(dp[3][i - 1] + arr[i] - dp[2][i - 1]))
-        {
-            // 이전에는 A가 가져감
-            dp[3][i] = dp[1][i - 1] + arr[i];
-            dp[2][i] = dp[0][i - 1];
-
-            diff = abs(dp[1][i - 1] + arr[i] - dp[0][i - 1]);
-        }
-        else
-        {
-            // 이전에도 B가 가져감
-            dp[3][i] = dp[3][i - 1] + arr[i];
-            dp[2][i] = dp[2][i - 1];
-
-            diff = abs(dp[3][i - 1] + arr[i] - dp[2][i - 1]);
-        }
-
-        // 이전 기록을 다 합치고 새로 만들어주는게 차이가 적은 경우
-        if (abs(dp[2][i - 1] + dp[3][i - 1] - arr[i]) < diff)
-        {
-            dp[3][i] = dp[3][i - 1] + dp[2][i - 1];
-            dp[2][i] = arr[i];
-        }
-
-
-               // cout << dp[0][i] << " " << dp[1][i] << " " << dp[2][i] << " " << dp[3][i] << endl;
+        cin >> arr[i];
+        sum += arr[i];
     }
 
-   // cout << dp[0][n - 1] << " " << dp[1][n - 1] << " " << dp[2][n - 1] << " " << dp[3][n - 1] << endl;
+    for(int i=0;i<100001;i++) dp[i] = -1;
+    dp[0] = 0;
 
-    cout << min(abs(dp[0][n - 1] - dp[1][n - 1]), abs(dp[2][n - 1] - dp[3][n - 1]));
+    for(int j=0;j<n;j++)
+    {
+        for(int i=100000;i>=0;i--)
+        {
+            if (i >= arr[j])
+            {
+                if (dp[i - arr[j]] == -1) continue;
 
+                dp[i] = 1;
+            }
+        }
+    }
+
+    
+    for(int i=1;i<100000;i++) 
+    {
+        if (dp[i] == 1) 
+        {
+            int b = sum - i;
+
+            ans = min(ans, abs(b - i));
+        }
+    }
+
+    cout << ans;
 
     return 0;
 }
